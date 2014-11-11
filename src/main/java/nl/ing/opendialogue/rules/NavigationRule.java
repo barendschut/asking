bf5895efb0060e55a47cb16fb7488db469d16890
Bart Boon <boon1970@gmail.com>
2014-11-11 15:49:50 +0100

    added navigation rule

---
+++ src/main/java/nl/ing/opendialogue/rules/NavigationRule.java
@@ -0,0 +1,48 @@
+package nl.ing.opendialogue.rules;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NavigationRule {
+
+	String context;
+	String question;
+
+	String nextContext;
+	String nextQuestion;
+
+	public NavigationRule(final String context, final String question, final String nextContext, final String nextQuestion) {
+		this.context = context;
+		this.question = question;
+		this.nextQuestion = nextQuestion;
+		this.nextContext = nextContext;
+	}
+	
+	
+	public List<NavigationRule> getNavigationRules() {
+		List<NavigationRule> navigationList = new ArrayList<NavigationRule>();
+		
+		navigationList.add(new NavigationRule("", "pas gevonden", "", ""));
+		
+		return navigationList;
+		
+	}
+	
+	
+	public String getContext() {
+		return context;
+	}
+
+	public String getQuestion() {
+		return question;
+	}
+
+	public String getNextContext() {
+		return nextContext;
+	}
+
+	public String getNextQuestion() {
+		return nextQuestion;
+	}
+
+}
